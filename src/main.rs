21b6f9424750bb5f6eec74ef50ab71eebde27287
Aza Walker <azawalker.jr@gmail.com>
2025-01-14 20:56:12 -0500

    adding type command and did a refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process;
 
 fn main() {
     loop {
+        let builtins = ["exit", "echo", "type"];
+
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match input.trim() {
-            input if input.starts_with("exit") => {
-                let tokens = input.split(" ").collect::<Vec<&str>>();
+        let tokens = input
+            .trim()
+            .split(" ")
+            .map(|t| t.to_string())
+            .collect::<Vec<String>>();
+        match tokens[0].as_str() {
+            "exit" => {
                 if tokens.len() < 2 {
                     println!("exit command expects integer");
                     continue;
@@ -25,10 +29,28 @@ fn main() {
                     Err(_) => println!("exit command expects integer"),
                 }
             }
-            input if input.starts_with("echo") => {
-                let chars = input.chars().collect::<Vec<char>>();
-                let echo = chars[5..].iter().collect::<String>();
+            "echo" => {
+                if tokens.len() < 2 {
+                    println!();
+                    continue;
+                }
+
+                let echo = tokens[1..].join(" ");
                 println!("{}", echo);
+            }
+            "type" => {
+                if tokens.len() < 2 {
+                    println!("exit command expects argument");
+                    continue;
+                }
+
+                let builtin = &tokens[1];
+                if !builtins.contains(&builtin.as_str()) {
+                    println!("{}: not found", builtin);
+                    continue;
+                }
+
+                println!("{} is a shell builtin", builtin);
             }
             _ => println!("{}: command not found", input.trim()),
         }
